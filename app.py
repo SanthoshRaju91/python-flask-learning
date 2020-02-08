12bb21c2483ca4a919f3499c3259cbcc05d191ad
santhoshRaju91 <santhosh.codeholic@gmail.com>
2020-02-08 13:22:52 +0530

    Using sessions

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, request, redirect, url_for, make_response
+from flask import Flask, render_template, request, redirect, url_for, session
 
 # __name__ -> This is python telling the application, the modules from package you need to use from
 # In flask, it gives a context of where the application is running and to look for files.
 app = Flask(__name__)
+
+app.secret_key = "my_secret_password"
 
 @app.route("/")
 def index():
@@ -21,12 +23,11 @@ def form():
     if request.method == "POST":
         first_name = request.values.get("first_name")
         last_name = request.values.get("last_name")
-        response = make_response(redirect(url_for("registered")))
-        response.set_cookie("first_name", first_name)
-        return response
+        session["first_name"] = first_name
+        return redirect(url_for("registered"))
     return render_template("form.html")
 
 @app.route("/thank-you")
 def registered():
-    first_name = request.cookies.get("first_name")
+    first_name = session.get("first_name")
     return f"Thank you for registering, {first_name}"
