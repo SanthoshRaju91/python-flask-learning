ef2e24f18e84587cf67e1953d49edc6f5a636f2e
santhoshRaju91 <santhosh.codeholic@gmail.com>
2020-02-08 13:17:23 +0530

    Web form with redirections

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 # __name__ -> This is python telling the application, the modules from package you need to use from
 # In flask, it gives a context of where the application is running and to look for files.
@@ -21,5 +21,9 @@ def form():
     if request.method == "POST":
         first_name = request.form.get("first_name")
         last_name = request.form.get("last_name")
-        return f"First name: {first_name}, Last name: {last_name}"    
+        return redirect(url_for("registered"))
     return render_template("form.html")
+
+@app.route("/thank-you")
+def registered():
+    return "Thank you for registering."
