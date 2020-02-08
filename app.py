7d9c783e1be7852eb12f456c800279974f194fdb
santhoshRaju91 <santhosh.codeholic@gmail.com>
2020-02-08 12:08:53 +0530

    Basic templating

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 # __name__ -> This is python telling the application, the modules from package you need to use from
 # In flask, it gives a context of where the application is running and to look for files.
@@ -6,12 +6,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Index page"
+    return render_template("index.html")
 
 @app.route("/hello/<name>")
 def hello_name(name):
-    return f"Hello, {name}"
+    return render_template("hello/name.html", t_name=name)
 
 @app.route("/hello/<int:planet>")
 def hello_planet(planet):
-    return f"Hello, planet #{planet}"
+    return render_template("hello/planet.html", t_planet=planet)
